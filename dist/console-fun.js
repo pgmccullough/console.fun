7f2a576efadfe58283bbefe62f8e0820f0aac91b
Patrick Glendon McCullough <p@mccullo.ug>
2023-07-06 21:26:44 -0400

    pad right message

--- dist/console-fun.js
+++ dist/console-fun.js
@@ -4,7 +4,7 @@ console.log = function (message) {
     var phase = 0;
     setInterval(function () {
         console.clear();
-        unfunLog("%c ".concat(message), phase++ % 2
+        unfunLog("%c ".concat(message, " "), phase++ % 2
             ? 'font-size:3rem;background:#ff3ac6;color:#5fa41c;'
             : 'font-size:3rem;background:#009b00;color:#ff1a00;');
     }, 100);
--- src/console-fun.ts
+++ src/console-fun.ts
@@ -3,7 +3,7 @@ console.log = (message: string): void => {
   let phase: number = 0;
   setInterval(() => {
     console.clear();
-    unfunLog(`%c ${message}`,
+    unfunLog(`%c ${message} `,
       phase++%2
         ?'font-size:3rem;background:#ff3ac6;color:#5fa41c;'
         :'font-size:3rem;background:#009b00;color:#ff1a00;'
